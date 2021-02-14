b336a7a03d48585071e9ee59c6f3163070cbcc17
nariman <mukhtarov.n@gmail.com>
2021-02-14 15:46:39 +0300

    HW1

---
+++ views.py
@@ -0,0 +1,11 @@
+from wavy import render
+
+
+def main_view(request):
+    secret = request.get('secret_key', None)
+    return '200 OK', render('index.html', secret=secret)
+
+
+def about_view(request):
+    secret = request.get('secret_key', None)
+    return '200 OK', render('about.html', secret=secret)
