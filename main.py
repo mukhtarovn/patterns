d5512a1df22af9984775560ea203871920fa1393
nariman <mukhtarov.n@gmail.com>
2021-03-01 22:16:33 +0300

    hw5

---
+++ main.py
@@ -0,0 +1,93 @@
+from wavy import render, Application, DebugApplication, FakeApplication
+from models import TrainingSite
+from logging_mod import Logger, debug
+
+
+site = TrainingSite()
+logger = Logger('main')
+
+
+def main_view(request):
+    logger.log('Список курсов')
+    return '200 OK', render('course_list.html', objects_list=site.courses)
+
+
+@debug
+def create_course(request):
+    if request['method'] == 'POST':
+        data = request['data']
+        name = data['name']
+        category_id = data.get('category_id')
+        category = None
+        if category_id:
+            category = site.find_category_by_id(int(category_id))
+
+            course = site.create_course('record', name, category)
+            site.courses.append(course)
+
+        return '200 OK', render('create_course.html')
+    else:
+        categories = site.categories
+        return '200 OK', render('create_course.html', categories=categories)
+
+
+def create_category(request):
+    if request['method'] == 'POST':
+
+        data = request['data']
+
+        name = data['name']
+
+        name = Application.decode_value(name)
+        category_id = data.get('category_id')
+
+        category = None
+        if category_id:
+            category = site.find_category_by_id(int(category_id))
+
+        new_category = site.create_category(name, category)
+
+        site.categories.append(new_category)
+
+        return '200 OK', render('create_category.html')
+    else:
+        categories = site.categories
+        return '200 OK', render('create_category.html', categories=categories)
+
+
+urlpatterns = {
+    '/': main_view,
+    '/create-course/': create_course,
+    '/create-category/': create_category
+}
+
+
+def secret_controller(request):
+    request['secret'] = 'secret'
+
+
+front_controllers = [
+    secret_controller
+]
+
+application = Application(urlpatterns, front_controllers)
+
+
+@application.add_route('/copy-course/')
+def copy_course(request):
+    request_params = request['request_params']
+    name = request_params['name']
+    old_course = site.get_course(name)
+    if old_course:
+        new_name = f'copy_{name}'
+        new_course = old_course.clone()
+        new_course.name = new_name
+        site.courses.append(new_course)
+
+    return '200 OK', render('course_list.html', objects_list=site.courses)
+
+
+@application.add_route('/category-list/')
+def category_list(request):
+    logger.log('Список категорий')
+    return '200 OK', render('category_list.html', objects_list=site.categories)
---
+++ reusepatterns/prototypes.py
@@ -0,0 +1,8 @@
+import copy
+
+
+class PrototypeMixin:
+
+    def clone(self):
+        """Clone a registered object and update inner attributes dictionary"""
+        return copy.deepcopy(self)
